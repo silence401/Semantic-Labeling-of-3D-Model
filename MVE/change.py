f95e08fa87a6658760365949ea6062d24ed756a5
silence401 <913725480@qq.com>
2020-09-07 21:16:59 +0800

    删除了一些旧内容

--- MVE/change.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import os
-lis = os.listdir("./views")
-for i in range(len(lis)):
-    img_path = "./views/"+os.path.join(lis[i],'label.png')
-    img = cv2.imread(img_path, -1)
-    print(img.dtype)
-    cv2.imwrite('./views/'+os.path.join(lis[i], 'label1.png'), img)
